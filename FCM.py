cfbcd4e8ea45ed1b553ac47bb04f00e8ede6302d
/dev/rsa <kris13@bk.ru>
2021-02-10 13:41:37 +0300

    Add FCM class features

--- FCM.py
+++ FCM.py
@@ -10,24 +10,63 @@ Based on OrangePWM - Python software PWM for Orange Pi
 https://github.com/evergreen-it-dev/orangepwm.git
 """
 
-class OrangePwm(threading.Thread):
 
-  def __init__(self, frequency, gpioPin, gpioScheme=0):
-     """ 
-     Init the OrangePwm instance. Expected parameters are :
-     - frequency : the frequency in Hz for the PWM pattern. A correct value may be 100.
-     - gpioPin : the gpio.port which will act as PWM ouput
-     - gpioScheme : saved for compatibility with PiZyPWM code
-     """
-     self.baseTime = 1.0 / frequency
-     self.maxCycle = 100.0
-     self.sliceTime = self.baseTime / self.maxCycle
-     self.gpioPin = gpioPin
-     self.terminated = False
-     self.toTerminate = False
-     #GPIO.setmode(gpioScheme)
-     GPIO.setmode(GPIO.SUNXI)
-     GPIO.setwarnings(False)
+class FCM(threading.Thread):
+
+  def __init__(self, frequency, channelOUT='PL8', channelIN=None):
+    """ 
+    Init the OrangePwm instance. Expected parameters are :
+    - frequency : the frequency in Hz for the PWM pattern. A correct value may be 100.
+    - channelOUT : the gpio.port which will act as PWM ouput
+    """
+    self.terminated = False
+    self.toTerminate = False
+    self.terminatedCounter = False
+    self.toTerminateCounter = False
+
+    self.initBaseValues(frequency)
+
+    self.Counter = 0
+    self.RPM = 0.0
+    self.RPMdelta = 10.0
+    self.lastTemp = 0.0
+
+    self.channelOUT=channelOUT
+    self.channelIN=channelIN
+
+    self.iter_start = time.perf_counter()
+
+    GPIO.setmode(GPIO.SUNXI)
+    GPIO.setwarnings(False)
+
+  def __del__(self):
+    if self.toTerminate == True:
+        self.stop()
+    GPIO.cleanup()
+
+  def initBaseValues(self, frequency, maxCycle=100.0):
+    self.baseTime = 1.0 / float(frequency)
+    self.maxCycle = maxCycle
+    self.sliceTime = self.baseTime / self.maxCycle
+
+  def getTemp(self):
+    with open('/etc/armbianmonitor/datasources/soctemp', "r") as tempfile:
+      temp = tempfile.read()
+      tempfile.close()
+      return float(temp)/1000.0
+    return 0.0
+
+  def getLastTemp(self):
+    return self.lastTemp
+
+  def getLastRPM(self):
+    return self.RPM
+
+  def getMaxCycle(self):
+    return self.maxCycle
+
+  def getdutyCycle(self):
+    return self.dutyCycle
 
   def start(self, dutyCycle):
     """
@@ -38,8 +77,21 @@ class OrangePwm(threading.Thread):
     stay HIGH for 1*(25/100) seconds on HIGH output, and 1*(75/100) seconds on LOW output.
     """
     self.dutyCycle = dutyCycle
-    GPIO.setup(self.gpioPin, GPIO.OUT)
+    GPIO.setup(self.channelOUT, GPIO.OUT)
+    self.lock = threading.Lock()
     self.thread = threading.Thread(None, self.run, None, (), {})
+    self.thread.start()
+
+    if self.channelIN is not None:
+        self.startCounter()
+
+  def startCounter(self):
+    """
+    Start PWM counter
+    """
+    GPIO.setup(self.channelIN, GPIO.IN)
+    self.lockCounter = threading.Lock()
+    self.thread = threading.Thread(None, self.runCounter, None, (), {})
     self.thread.start()
 
 
@@ -47,16 +99,53 @@ class OrangePwm(threading.Thread):
     """
     Run the PWM pattern into a background thread. This function should not be called outside of this class.
     """
+    self.lock.acquire()
     while self.toTerminate == False:
       if self.dutyCycle > 0:
-        GPIO.output(self.gpioPin, GPIO.HIGH)
+        if GPIO.input(self.channelOUT) == GPIO.LOW:
+            GPIO.output(self.channelOUT, GPIO.HIGH)
         time.sleep(self.dutyCycle * self.sliceTime)
-
+ 
       if self.dutyCycle < self.maxCycle:
-        GPIO.output(self.gpioPin, GPIO.LOW)
+        if GPIO.input(self.channelOUT) == GPIO.HIGH:
+            GPIO.output(self.channelOUT, GPIO.LOW)
         time.sleep((self.maxCycle - self.dutyCycle) * self.sliceTime)
 
-    self.terminated = True
+      if self.channelIN is not None:
+        now = time.perf_counter()
+        iter_elapsed = now - self.iter_start
+
+        if iter_elapsed > self.RPMdelta:
+          self.iter_start = now
+          self.countRPM(iter_elapsed)
+
+    self.lock.release()
+
+  def runCounter(self):
+    """
+    Run the PWM counter.
+    """
+    self.lockCounter.acquire()
+
+    while self.toTerminateCounter == False:
+      GPIO.wait_for_edge(self.channelIN,GPIO.FALLING, timeout=1000)
+      self.doCounter()
+
+#    GPIO.remove_event_detect(self.channelIN)
+    self.lockCounter.release()
+
+  def doCounter(self):
+    self.Counter += 1
+
+  def countRPM(self, delta):
+    """
+    count RPM of
+    """
+    tempCounter=float(self.Counter)
+    self.Counter = 0
+    self.RPM = tempCounter/delta*60.0
+    self.lastTemp = self.getTemp()
+#    print(self.RPM, self.getTemp())
 
 
   def changeDutyCycle(self, dutyCycle):
@@ -87,9 +176,27 @@ class OrangePwm(threading.Thread):
     Stops PWM output.
     """
     self.toTerminate = True
-    while self.terminated == False:
-      # Just wait
-      time.sleep(0.01)
- 
-    GPIO.output(self.gpioPin, GPIO.LOW)
-    GPIO.setup(self.gpioPin, GPIO.IN)
+
+    self.lock.acquire()
+
+    GPIO.output(self.channelOUT, GPIO.LOW)
+    if self.channelIN is not None:
+        self.stopCounter()
+
+    self.lock.release()
+    GPIO.cleanup(self.channelOUT)
+
+    self.toTerminate = False
+
+
+  def stopCounter(self):
+    """
+    Stops Counter.
+    """
+    self.toTerminateCounter = True
+    self.lockCounter.acquire()
+
+
+    self.lockCounter.release()
+    GPIO.cleanup(self.channelIN)
+
